af4bfb62fd51fcf07e73406ad3d1444a13361e40
Computhink-CS144 <cs144@computhink.com.sg>
2026-01-18 04:25:34 +0000

    end of lesson 2

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,7 +1,33 @@
 print("Hello from lesson 2")
+# print("Red")
+# print("Orange")
+# print("yellow")
+print("green")
+# print("blue")
+# print("purple")
+# print("pink")
 
-######## Write the pseudocode in comments for task 2 here
-# Using comments, translate the code shown on screen into pseudocode.
+# counter = 0
+# repeat until counter = 50
+# move 10 steps
+# turn 15 degrees
+# change counter by 10
+# what is your age?
+# if awnser < 18
+# say access denied
+# else
+# say welcome!
 
-######## Write the pseudocode in comments for task 3 here
-# Using comments, translate the code shown on screen into pseudocode.
+# forever
+#   accept one item from the customer
+#   use the sensor to check the matefrial
+#   if it is glass then
+#     xxx
+#   ???
+#   otherwise
+#     reject the item
+
+# password to IDK
+# ask person : please enter password
+# person enters "password"
+# say "access denied"
