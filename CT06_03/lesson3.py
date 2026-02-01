a88916c4e87382cb4f19f4b6056b4ef392c408df
Computhink-CS144 <cs144@computhink.com.sg>
2026-02-01 03:30:26 +0000

    end of lesson 3

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,35 @@
-print("Hello from lesson 3")
+# print("Hello from lesson 3")
+# print("My scentence")
+# age = 10
+# print("age")
+# print(age)
+# x = 10
+# age = age + x
+# print(age)
+# print("when i am 20 yrs old , i want to join brawl stars Esports")
+# variable1 = 987654321
+# variable2 = 12345679
+# print( variable1 + variable2 )
+# print( variable1 - variable2 )
+# print( variable1 * variable2 )
+# print( variable1 / variable2 )
+# print( variable1 ** variable2 )
+# print( round(45.6, 0))
+# var1 = 42.56789
+# var2 = 1267.15
+# var3 = 1785.956
+# print(round(var1, 2))
+# print(round(var2))
+# print(round(var3, -2))
+# a = 2
+# b = 3
+# print( a + b)
+# print(( 50 - 10 ) *3)
+# print(100/4)
+# print((3+4)*5-(10/2))
+print( 2 * 15 )
+total = 96 + (3*18) + (6*2)
+service_charge = 10/100 * total
+total = total + service_charge
+total = total -25
+print(total)
