de5c49a4300449a5f3c108cc662b1a20b2e15c12
Computhink-CS144 <cs144@computhink.com.sg>
2026-03-08 03:07:56 +0000

    end of lesson

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,2 +1,62 @@
-print("Hello from lesson 7")
-
+# print("Hello from lesson 7")
+# score_one = 80
+# score_two = 90
+# score_three = 75
+# total = score_one + score_two + score_three
+# average_score = total / 3
+# student_name = "Alex"
+# print("Average score for " + student_name + " is: " + str(round(average_score, 2)))
+#***Task 1***
+# for i in range(1,11):
+#     print(i)
+#***Task 2***
+# for i in range(2,21,2):
+#     print(i)
+#***Task 3***
+# for i in range(10,0,-1):
+#      print(i)
+#***Task 4***
+# word = input("what word do you want to repeat? ")
+# number_times = input("how many times do you want to repeat it? ")
+# for i in range(1, int(number_times) + 1):
+#     print(word)
+#***Task 5***
+# word = input("what is your name? ")
+# number_times = input("how many times do you want to repeat it? ")
+# for i in range(1, int(number_times) + 1):
+#     print("nice to meet you " + word)
+#***Task 6***
+# number1 = input("enter a number: ")
+# number2 = input("enter another number: ")
+# number3 = input("enter a third number: ")
+# number4 = input("enter a fourth number: ")
+# number5 = input("enter a fifth number: ")
+# total = int(number1) + int(number2) + int(number3) + int(number4) + int(number5)
+# print("the total of the numbers you entered is: " + str(total))
+#                 OR
+# for count in range(1, 6):
+#     number(count) = input("enter a number: ")
+# total = int(number1) + int(number2) + int(number3) + int(number4) + int(number5)
+# print("the total of the numbers you entered is: " + str(total)) 
+#***Task 7***
+# number = input("enter a number for a times table you want: ")
+# for i in range(1, 13):
+#     print(str(number) + " x " + str(i) + " = " + str(int(number) * i))
+#anum = int( input(which times table do you want? "))
+#for count in range(1, 13):
+#    print(anum, "x", count, "=", num * count)
+#***Task 8***
+# a = 0
+# for i in range(1, 6):
+#      a = a + 1
+#      for b in range(a):
+#          print(a)
+#***Task 9***
+# num1 = input("enter student 1's grade: ")
+# num2 = input("enter student 2's grade: ")
+# num3 = input("enter student 3's grade: ")
+# num4 = input("enter student 4's grade: ")
+# num5 = input("enter student 5's grade: ")
+# total = int(num1) + int(num2) + int(num3) + int(num4) + int(num5)
+# average = total / 5
+# print("the average grade for the students is: " + str(round(average, 2)))
