89aab0331cde20fd2f145054c00076fc624963a6
Computhink-CS144 <cs144@computhink.com.sg>
2026-03-15 04:25:23 +0000

    end of lesson 8

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -1,2 +1,70 @@
-print("Hello from lesson 8")
+# print("Hello from lesson 8")
 
+#         AWNSER for Recap 1:
+# results = 1
+# for count in range(1, 5+1):
+#     awnser = input("give me a number #" + str(count) + ": ")
+#     results = results *int(awnser)
+# print("The product of the 5 numbers is: " + str(results))
+
+
+#          AWNSER for task 1(A):
+# import time
+# for count in range (10 , 0 , -1):
+#     print(count)
+#     time.sleep(1)
+# print("Happy New Year!")
+
+
+#          AWNSER for task 1(B):
+# import time
+# count_from = input("Count from: ")
+# for count in range (int(count_from) , 0 , -1):
+#     print(count)
+#     time.sleep(1)
+# print("*explosion sounds*")
+
+
+#           AWNSER for task 2(A):
+# import random
+# number = random.randint(1, 6)
+# print(number)
+
+
+#           AWNSER for task 2(B):
+# import random
+# for count in range(1,21):
+#      number = random.randint(0,9999)
+#      print(number)
+
+
+
+#         AWNSER for task 3(A):
+# raining = input("Is it raining? (yes/no): ")
+# print("Is it raining? " + raining)
+
+
+#         AWNSER for task 3(B):
+# rain = True
+# umbrella = True
+# umbrella = rain
+
+
+#         AWNSER for task 3(C):
+# raining = input("Is it raining? (yes/no): ")
+# if raining == "yes":
+#     print("Take an umbrella!")
+# else:
+#     print("Enjoy the sunshine!")
+
+
+#         AWNSER for task 4(A)
+# import random
+# num1 = random.randint(1, 50)
+# num2 = random.randint(1, 50)
+# print("What is " + str(num1) + " + " + str(num2) + "?")
+# answer = input("Your answer: ")
+# if int(answer) == num1 + num2:
+#     print("Correct!")
+# else:
+#     print("Incorrect. The correct answer is " + str(num1 + num2) + ".")
