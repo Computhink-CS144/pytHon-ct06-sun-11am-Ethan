576dbacb0fef6dde880075e58a8c9a09b0d822eb
Computhink-CS144 <cs144@computhink.com.sg>
2026-04-12 03:03:02 +0000

    done

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,64 @@
-print("Hello from lesson 10")
+# print("Hello from lesson 10")
+
+#          recap 
+# import random
+# randint = random.randint(1, 15)
+# guess = int(input("Guess a number between 1 and 15: "))
+# if guess == randint:
+#     print("Congratulations! You guessed the number.")
+# else:
+#     print(f"Sorry, the number was {randint}. Better luck next time!")
+
+#           task 1
+# num = input("Enter a number: ")
+# if (int(num) > 0):
+#     print("The number is positive.")
+# else:
+#     print("The number is negative.")
+
+#           task 2
+# age = int(input("Enter your age: "))
+# if age < 13:
+#     print("You are a child.")
+# else:
+#     if age < 20:
+#         print("You are a teenager.")
+#     else:
+#         print("You are an adult.")
+
+#           task 3
+# OUTSIDE_TEMP = int(input("Enter the outside temperature in degrees Celsius: "))
+# if OUTSIDE_TEMP < 20:
+#     print("It's cold outside. just read a book.")
+# elif OUTSIDE_TEMP < 24 and OUTSIDE_TEMP >= 20:
+#     print("It's a nice day outside. Let's go and cycle.")
+# elif OUTSIDE_TEMP < 30 and OUTSIDE_TEMP >= 25:
+#     print("let's play basketball outside.")
+# else:
+#     print("It's too hot outside. Let's go swimming.")
+
+#          task 4
+# score = int(input("Enter your score: "))
+# if score >= 90:
+#     print("Your grade is A.")
+# elif score >= 80 and score < 90:
+#     print("Your grade is B.")
+# elif score >= 70 and score < 80:
+#     print("Your grade is C.")
+# elif score >= 60 and score < 70:
+#     print("Your grade is D.")
+# else:
+#     print("Your grade is F.")
+
+#                    OR
+# score = int(input("Enter your score: "))
+# if score in range(90, 101):
+#     print("Your grade is A.")
+# elif score in range(80, 90):
+#     print("Your grade is B.")
+# elif score in range(70, 80):
+#     print("Your grade is C.")
+# elif score in range(60, 70):
+#     print("Your grade is D.")
+# else:
+#     print("Your grade is F.")
--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,84 @@
-print("Hello from lesson 11")
+# print("Hello from lesson 11")
+
+#                     Task 1
+# rider1height = int(input("What is the height of rider 1 in cm? "))
+# rider2height = int(input("What is the height of rider 2 in cm? "))
+# if rider1height > 120 and rider2height > 120:
+#     print("You can ride together!")
+
+
+#                     Task 2
+# num = int(input("Enter a number: "))
+# if num % 3 == 0:
+#     if num % 7 == 0:
+#         print("The number is divisible by both 3 and 7.")
+#     else:
+#         print("The number is divisible by 3 but not by 7.")
+# else:
+#     print("The number is not divisible by 3 or 7.")
+
+
+#                     Task 3
+# firstname = input("What is your first name? ")
+# lastname = input("What is your last name? ")
+# if firstname == "James" and lastname == "leong":
+#     print("You are WANTED,  James!")
+# else:
+#     print("You are not wanted, ", firstname, lastname)
+
+
+#                    Task 4
+# rider1age = int(input("What is the age of rider 1? "))
+# rider2age = int(input("What is the age of rider 2? "))
+# if rider1age > 18 or rider2age > 18:
+#     print("You can ride!")
+# else:
+#     print("Sorry, you cannot ride.")
+
+
+#                    Task 5
+# age = int(input("What is your age? "))
+# if age < 12 or age > 65:
+#     print("You get a discount! You only have to pay $15.")
+# else:
+#     print("Sorry, you do not get a discount. You have to pay $20.")
+
+
+#                    Task 6
+# GENDER = input("What is your gender? (M/F) ")
+# if GENDER == "M" or GENDER == "m":
+#     print("please proceed")
+# else:
+#     print("GET OUT!!!")
+
+
+#                     Task 7
+# import random
+# number = random.randint(1, 7)
+# if number == 1:
+#     colourcode = "red"
+# elif number == 2:
+#     colourcode = "orange"
+# elif number == 3:
+#     colourcode = "yellow"
+# elif number == 4:
+#     colourcode = "green"
+# elif number == 5:
+#     colourcode = "blue"
+# elif number == 6:
+#     colourcode = "purple"
+# else:
+#     colourcode = "pink"
+# guessedcolour = input("Guess the colour: ")
+# if  not (guessedcolour  == colourcode):
+#     print("Wrong! The colour was ", colourcode + ". You stupid idiot!")
+#else:
+#     print("Correct! You are a "genius"!")
+
+
+#                  Task 8
+# day = input("What day is it today? ")
+# if not (day == "saturday" or day == "sunday"):
+#     print("It's a weekday.")
+# else:
+#     print("It's a weekend.")
